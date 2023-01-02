c2eccd6c61d3cb27524452912e7b0b6e8be2e77e
aryanawasthi <36572381+aryanawasthi@users.noreply.github.com>
2023-01-02 13:20:18 +0530

    Add files via upload
    Introduction of SQLite and SQLAlchment with Python

---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlalchemy as db
+engine=db.create_engine('sqlite:///movies.db')
+connection=engine.connect()
+metadata=db.MetaData()
+movies=db.Table('Movies',metadata,autoload=True,autoload_with=engine)
+query=db.select([movies])
+result_proxy=connection.execute(query)
+result_set=result_proxy.fetchall()
+print(result_set[0:])
+query=movies.select(movies.columns.year>2000)
+result_proxy=connection.execute(query)
+result_set=result_proxy.fetchall()
+print(result_set)
+query=movies.select().where(movies.columns.director=="Wes Anderson")
+result_proxy=connection.execute(query)
+result_set=result_proxy.fetchall()
+print(result_set)
+
